2104728740a328d800f5c582c9ba41fb22bfc0f5
Komal Pathare <117629456+komalpathare8755@users.noreply.github.com>
2023-12-24 15:34:24 +0530

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,19 @@
+//Comparison Operator
+//->Relational Operator
+//   > Greater then
+//    < less than
+//    >= Grater then or equal to
+//    <= Less than or equal to
+//console.log(10 > 10);
+//console.log(10 < 10);
+//console.log(10 >= 10);
+//console.log(10 <= 10);
+//->Equality Operator
+// === strict equality (type+value)
+// !== strict non-equality operators (type+value)
+//  == lose equality operator (value)
+//  != losenot equality operator
+//console.log(10 === 10);
+//console.log(10 !== 10);
+console.log(10 == 10);
+console.log(10 != 10);
